7e184bed04780ff6b5fc22e7bfe33ca2e43d9a9b
DonovanSaberhagen <jimenez.javier14@gmail.com>
2024-04-17 19:58:20 -0400

    Ajustando Detalles

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
+
+$(document).ready(function () {
+
+  $(".card-img-top").click(function () {
+    $(".list-group-item").toggle();
+  });
+
+  $("button").click(function () {
+    console.log("HiceClick");
+    alert("Mensaje fue Enviado");
+  });
+
+  $("h3").on("dblclick", function () {
+    console.log("HiceClick");
+    $(this).css({ color: "yellow" });
+  });
+
+  
+});
